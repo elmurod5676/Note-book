92b2736e68cf587797f460797a7df3aebc0a6dd3
elmurod <e937295456@gmail.com>
2023-11-02 23:09:54 +0500

    Add project files.

---
+++ OneNote/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace OneNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.previousNotes = new System.Windows.Forms.DataGridView();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.newnoteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExitLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(464, 51);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(497, 22);
+            this.titleBox.TabIndex = 0;
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(464, 100);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(497, 544);
+            this.noteBox.TabIndex = 1;
+            // 
+            // previousNotes
+            // 
+            this.previousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.previousNotes.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.previousNotes.Location = new System.Drawing.Point(12, 32);
+            this.previousNotes.Name = "previousNotes";
+            this.previousNotes.RowHeadersWidth = 51;
+            this.previousNotes.RowTemplate.Height = 24;
+            this.previousNotes.Size = new System.Drawing.Size(446, 505);
+            this.previousNotes.TabIndex = 2;
+            this.previousNotes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previousNotes_CellDoubleClick);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton.ForeColor = System.Drawing.Color.Goldenrod;
+            this.loadButton.Location = new System.Drawing.Point(12, 543);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(224, 49);
+            this.loadButton.TabIndex = 3;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.ForeColor = System.Drawing.Color.Goldenrod;
+            this.deleteButton.Location = new System.Drawing.Point(242, 543);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(216, 49);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveButton.ForeColor = System.Drawing.Color.Goldenrod;
+            this.SaveButton.Location = new System.Drawing.Point(242, 595);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(216, 49);
+            this.SaveButton.TabIndex = 5;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // newnoteButton
+            // 
+            this.newnoteButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.newnoteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newnoteButton.ForeColor = System.Drawing.Color.Goldenrod;
+            this.newnoteButton.Location = new System.Drawing.Point(12, 595);
+            this.newnoteButton.Name = "newnoteButton";
+            this.newnoteButton.Size = new System.Drawing.Size(224, 49);
+            this.newnoteButton.TabIndex = 6;
+            this.newnoteButton.Text = "New note";
+            this.newnoteButton.UseVisualStyleBackColor = true;
+            this.newnoteButton.Click += new System.EventHandler(this.newnoteButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(-3, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "OneNote";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Note";
+            // 
+            // ExitLabel
+            // 
+            this.ExitLabel.AutoSize = true;
+            this.ExitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitLabel.ForeColor = System.Drawing.Color.Red;
+            this.ExitLabel.Location = new System.Drawing.Point(950, 1);
+            this.ExitLabel.Name = "ExitLabel";
+            this.ExitLabel.Size = new System.Drawing.Size(21, 20);
+            this.ExitLabel.TabIndex = 10;
+            this.ExitLabel.Text = "X";
+            this.ExitLabel.Click += new System.EventHandler(this.ExitLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(973, 647);
+            this.Controls.Add(this.ExitLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newnoteButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.previousNotes);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.titleBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.DataGridView previousNotes;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button newnoteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ExitLabel;
+    }
+}
+
---
+++ OneNote/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneNote
+{
+    public partial class Form1 : Form
+    {
+        DataTable notes = new DataTable();
+        bool editing = false;
+        int rowIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
+
+            previousNotes.DataSource = notes;
+
+            CleanTextBoxes();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = DialogResult.No;
+
+            if (!string.IsNullOrWhiteSpace(titleBox.Text) &&
+                    !string.IsNullOrWhiteSpace(noteBox.Text) &&
+                    editing)
+            {
+                dialogResult = MessageBox.Show("You have some unsaved info. do you want to save them ?", "Confirm", MessageBoxButtons.YesNo);
+            }
+
+
+            if (dialogResult == DialogResult.No)
+            {
+
+                if (rowIndex >= 0 &&
+                        notes.Rows.Count > rowIndex)
+                {
+                    notes.Rows[rowIndex].Delete();
+                    rowIndex = -1;
+                    editing = false;
+                }
+                else
+                    Console.WriteLine("Not a valid note");
+            }
+            else
+            {
+                notes.Rows[rowIndex]["Title"] = titleBox.Text;
+                notes.Rows[rowIndex]["Note"] = noteBox.Text;
+
+                CleanTextBoxes();
+
+                rowIndex = -1;
+                editing = false;
+            }
+        }
+    
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (rowIndex >= 0 && notes.Rows.Count > rowIndex)
+            {
+                titleBox.Text = "" + notes.Rows[rowIndex][0];
+                noteBox.Text = "" + notes.Rows[rowIndex][1];
+            }
+            else
+                MessageBox.Show("Qator tanlanmagan !");
+        }
+
+        private void CleanTextBoxes()
+        {
+            titleBox.Text = string.Empty;
+            noteBox.Text = string.Empty;
+        }
+
+        private void newnoteButton_Click(object sender, EventArgs e)
+        {
+            CleanTextBoxes();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (editing)
+            {
+                if (!string.IsNullOrWhiteSpace(titleBox.Text) &&
+                       !string.IsNullOrWhiteSpace(noteBox.Text))
+                {
+                    notes.Rows[rowIndex]["Title"] = titleBox.Text;
+                    notes.Rows[rowIndex]["Note"] = noteBox.Text;
+
+                    CleanTextBoxes();
+
+                    rowIndex = -1;
+                    editing = false;
+                }
+                else
+                {
+                    MessageBox.Show("Ma'lumotlarni to'liq kiriting !");
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrWhiteSpace(titleBox.Text) &&
+                    !string.IsNullOrWhiteSpace(noteBox.Text))
+                    notes.Rows.Add(titleBox.Text, noteBox.Text);
+
+                CleanTextBoxes();
+
+                editing = false;
+            }
+
+        }
+
+        private void previousNotes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rowIndex = previousNotes.CurrentCell.RowIndex;
+
+            if (rowIndex >= 0 && notes.Rows.Count > rowIndex)
+                editing = true;
+        }
+
+        private void ExitLabel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
